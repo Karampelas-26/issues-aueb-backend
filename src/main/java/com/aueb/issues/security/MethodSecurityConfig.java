7ddd72bba38c3a0f0b62ea513e053f9d099ae2dc
George Karampelas <george.karampelas.26@gmail.com>
2023-05-24 21:49:26 +0300

    add globalMethodSecurity

---
+++ src/main/java/com/aueb/issues/security/MethodSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.aueb.issues.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+/**
+ * @author George Karampelas
+ */
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+}
