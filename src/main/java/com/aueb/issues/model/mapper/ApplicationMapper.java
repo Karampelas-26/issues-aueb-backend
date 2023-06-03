407da1880190c2f39de8e86e29f7dff7564fce76
George Karampelas <george.karampelas.26@gmail.com>
2023-06-03 16:41:34 +0300

    refactor user

---
+++ src/main/java/com/aueb/issues/model/mapper/ApplicationMapper.java
@@ -0,0 +1,25 @@
+package com.aueb.issues.model.mapper;
+
+import com.aueb.issues.model.entity.ApplicationEntity;
+import com.aueb.issues.web.dto.ApplicationDTO;
+import com.aueb.issues.web.dto.TeacherApplicationsDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import java.util.List;
+@Mapper
+public interface ApplicationMapper {
+    ApplicationMapper INSTANCE = Mappers.getMapper(ApplicationMapper.class);
+
+    @Mapping(source = "entity.id", target = "id")
+    @Mapping(source = "entity.title", target = "title")
+    @Mapping(source = "entity.site.name", target = "siteName")
+    @Mapping(source = "entity.site.building.name", target = "buildingName")
+    @Mapping(source = "entity.priority.name", target = "priority")
+    @Mapping(source = "entity.status", target = "status")
+    @Mapping(source = "entity.description", target = "description")
+    @Mapping(expression = "java(issueType.name())", target = "issueType")
+    @Mapping(source = "entity.dueDate", target = "dueDate")
+    public ApplicationDTO toDTO(ApplicationEntity entity);
+}
---
+++ src/main/java/com/aueb/issues/web/dto/UserMapper.java
@@ -0,0 +1,25 @@
+package com.aueb.issues.web.dto;
+
+import com.aueb.issues.model.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phone", target = "phone")
+    @Mapping(source = "firstname", target = "firstname")
+    @Mapping(source = "lastname", target = "lastname")
+    @Mapping(source = "gender", target = "gender")
+    @Mapping(source = "address", target = "address")
+    @Mapping(source = "createdDate", target = "createdDate")
+    @Mapping(source = "role", target = "role")
+    UserDTO toDto(UserEntity userEntity);
+
+
+
+}
